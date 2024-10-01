a5d5a21d44f11937b4ca66ab31c3ac9a8b1b3c54
AlexeyZ <azv99@yandex.ru>
2024-10-01 18:18:44 +0300

    Реализовал отправку сообщения через отдельный сервер для большей безопасности

--- js/tg_message_service.js
+++ js/tg_message_service.js
@@ -1,53 +1,110 @@
 
+    // ВАРИАНТ БЕЗ ИСПОЛЬЗОВАНИЯ СЕРВЕРА ДЛЯ ОБРАБОТКИ СООБЩЕНИЙ
 
-
-function showPopup(){
-
-    const element = document.getElementById('alertPop-up');
-    element.show();
-  }
-
-function closePopup(){
-    const element = document.getElementById('alertPop-up');
-    element.close();
-  }
-    
     const TOKEN = "7518521452:AAGQvr3b5Q3fcOqHbmHMOVJkT9dsPLm0msQ";
     const CHAT_ID = "-1002318470204";
     const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
 
+    // const URI_SERVER = 'http://localhost:3000/telegram-message';   //Раскоментировать если используем сервер
+
+
     document.getElementById('tg-form').addEventListener('submit', function(e) {
         e.preventDefault();
         let text = document.querySelector('#msg');
+        const elementPopup = document.getElementById('alertPop-up');
 
-        let message = `<b>Заявка с сайта</b>\n`;
+        let content  = document.getElementById('alertContent');
+        
+
+       let message = `<b>Заявка с сайта</b>\n`;
         message += `<b>Отправитель:</b> ${this.name.value}\n`;
         message += `<b>Телефон:</b> ${this.phone.value}\n`;
         message += `<b>Сообщение:</b> ${text.value}\n`;
 
 
-
-        console.log(message)
-
-
-        axios.post(URI_API, {
-            chat_id: CHAT_ID,
-            parse_mode: 'html',
-            text: message
-        })
-        .then((res) => {
-            this.name.value = "";
-            this.phone.value = "";
-            text.value = "";
-
-            showPopup();
+        if (this.name.value === '' || this.phone.value === '' || text.value === '') {
+            content.innerHTML = `<h1>Все поля должны быть заполнены!</h1>`;
+            elementPopup.style.borderColor = "red";
+            elementPopup.show();
+            
             setTimeout(() => {
-                closePopup();
+                elementPopup.close();
+                elementPopup.style.borderColor = "";
             }, 2000);
-        })
-        .catch((err) => {
-            console.warn(err);
-        })
-        .finally(() => {})
+        } else {
+            axios.post(URI_API, {
+                chat_id: CHAT_ID,
+                parse_mode: 'html',
+                text: message
+            })
+            .then((res) => {
+
+                    this.name.value = "";
+                    this.phone.value = "";
+                    text.value = "";
+
+                    content.innerHTML = `<h1>Сообщение отправлено!</h1>`;
+                    elementPopup.show()
+
+                setTimeout(() => {
+                    elementPopup.close();
+                }, 2000);
+            })
+            .catch((err) => {
+                console.warn(err);
+            })
+            .finally(() => {})
+        }
+        
     })
 
+
+     // ВАРИАНТ С ИСПОЛЬЗОВАНИЕМ СЕРВЕРА
+
+
+    // document.getElementById('tg-form').addEventListener('submit', function(e) {
+    //     e.preventDefault();
+    //     let text = document.querySelector('#msg');
+    //     const elementPopup = document.getElementById('alertPop-up');
+
+    //     let content  = document.getElementById('alertContent');
+
+    //     axios.post(URI_SERVER, {
+
+    //         name: this.name.value,
+    //         phone: this.phone.value,
+    //         message: text.value
+    //         })
+    //         .then((res) => {
+    //             console.log(res);
+
+    //             if(res.statusText === 'No Content') {
+    //                 content.innerHTML = `<h1>Все поля должны быть заполнены!</h1>`;
+    //                 elementPopup.style.borderColor = "red";
+    //                 elementPopup.show();
+                    
+    //                 setTimeout(() => {
+    //                     elementPopup.close();
+    //                     elementPopup.style.borderColor = "";
+    //                 }, 2000);
+    //             } else {
+
+    //                 this.name.value = "";
+    //                 this.phone.value = "";
+    //                 text.value = "";
+
+    //                 content.innerHTML = `<h1>Сообщение отправлено!</h1>`;
+    //                 elementPopup.show();
+                    
+    //                 setTimeout(() => {
+    //                     elementPopup.close();
+    //                 }, 2000);
+    //             }
+                
+    //         })
+    //         .catch((err) => {
+    //             console.warn(err);
+    //         })
+    //         .finally(() => {})
+
+    // })
